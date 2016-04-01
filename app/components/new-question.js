d5fe73e48b4f38cc36676312c12dee8e441d6517
Adam Ross Russell <Guest@epicodus-19.local>
2016-04-01 13:58:33 -0700

    add styling to the main nav and jumbotron and background

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    addNewQuestion(){
+        var params = {
+          name: this.get('name'),
+          content: this.get('content'),
+          image: this.get('image')
+        };
+        this.sendAction('addNewQuestion2', params);
+      }
+    }
 });
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -11,12 +11,17 @@ export default Ember.Route.extend({
       this.transitionTo('admin');
     },
     delete(question) {
-      if(confirm("Are you sure you would like to delete this question?")) {
-        question.destroyRecord();
-        this.transitionTo('admin');
+      if(confirm("Are you sure you would like to delete this question? It will also delete all of it's comments.")) {
+        var answer_deletions = question.get('answers').map(function(answer) {
+          return answer.destroyRecord();
+        });
+        Ember.RSVP.all(answer_deletions).then(function() {
+          return question.destroyRecord();
+        });
+        this.transitionTo('index');
       }
     },
-    saveQuestion3(question) { 
+    saveQuestion3(question) {
       question.save();
       this.transitionTo('admin');
     }
