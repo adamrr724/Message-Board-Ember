9035b3b516035b49287caad23c1ca06650fe8620
Adam Ross Russell <Guest@epicodus-19.local>
2016-04-01 08:27:02 -0700

    add models for question and answer

--- app/models/question.js
+++ app/models/question.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  name: DS.attr(),
+  question: DS.attr(),
+  image: DS.attr(),
+  answer: DS.hasMany('answer', {async: true})
 });
