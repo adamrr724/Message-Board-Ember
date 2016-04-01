3e72cfb218c09f040e4d1b7c0ed61b473c67d00e
Adam Ross Russell <Guest@epicodus-19.local>
2016-04-01 13:17:12 -0700

    beginning to build out admin page

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('question');
+  },
+  actions: {
+    addNewQuestion3(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('admin');
+    },
+    delete(question) {
+      if(confirm("Are you sure you would like to delete this question?")) {
+        question.destroyRecord();
+        this.transitionTo('admin');
+      }
+    },
+    saveQuestion3(question) { 
+      question.save();
+      this.transitionTo('admin');
+    }
+  }
 });
