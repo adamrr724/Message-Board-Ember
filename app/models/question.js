ea5cd507763d0bb921a5edf04137dea7ef8cfbd6
Adam Ross Russell <Guest@epicodus-19.local>
2016-04-01 10:23:41 -0700

    finally fixed firebase issue

--- app/models/question.js
+++ app/models/question.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  name: DS.attr(),
+  content: DS.attr(),
+  image: DS.attr(),
+  answers: DS.hasMany('answer', {async: true})
 });
