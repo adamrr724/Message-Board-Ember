f06219cb4f1c99a24217fbd61d395c2d2799ef3c
Adam Ross Russell <Guest@epicodus-19.local>
2016-04-01 10:40:27 -0700

    add models and answers to route in index

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-  return this.store.findAll('question');
+  model(){
+    return  Ember.RSVP.hash({
+      question: this.store.findAll('question'),
+      answer: this.store.findAll('answer')
+    });
   }
 });
